e5dbfec70df69710fefb264cde6286be01d5ea62
UltimateScrub <97784362+UltimateScrub@users.noreply.github.com>
2022-09-13 14:14:18 +0800

    Create PrintinASpecifiedFile.java

---
+++ Part 04/Exercise 25 - Printing a Specified File/PrintinASpecifiedFile.java
@@ -0,0 +1,18 @@
+public class PrintingASpecifiedFile {
+ 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+ 
+        System.out.println("Which file should have its contents printed?");
+        String file = scanner.nextLine();
+ 
+        try (Scanner fileReader = new Scanner(Paths.get(file))) {
+            while (fileReader.hasNextLine()) {
+                System.out.println(fileReader.nextLine());
+            }
+        } catch (Exception e) {
+            System.out.println("Reading the file failed.");
+        }
+ 
+    }
+}
